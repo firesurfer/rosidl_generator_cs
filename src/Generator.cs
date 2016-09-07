e5029f8be9b03dff2af38a4fc4e33de650c98a4c
firesurfer <firesurfer65@yahoo.de>
2016-09-07 09:01:50 +0200

    Stated implementation of a message wrapper so there won't be any need for manual memory handling

--- src/Generator.cs
+++ src/Generator.cs
@@ -9,6 +9,18 @@ using Microsoft.CSharp;
 using System.CodeDom.Compiler;
 namespace ROS2CSMessageGenerator
 {
+	public class MessageMember
+	{
+		public string name;
+		public string type;
+		public string default_init;
+		public bool isArray = false;
+		public override string ToString ()
+		{
+			string ret = "public " +type  + " " + name + ";";
+			return ret;
+		}
+	}
 	public class CsClassGenerator
 	{
 		StreamReader FileReader;
@@ -17,11 +29,13 @@ namespace ROS2CSMessageGenerator
 		public string Name{ get; private set; }
 
 		List<string> Members = new List<string>();
+		List<MessageMember> MessageMembers = new List<MessageMember>();
 		StringBuilder ClassString =  new StringBuilder();
+		StringBuilder WrapperClassString = new StringBuilder();
 		bool IsService = false;
 
 		bool ClassWasFinalized = false;
-
+		private string StructName;
 		public CsClassGenerator (string _Path, string _PackageName)
 		{
 			FileReader = new StreamReader (_Path);
@@ -37,7 +51,15 @@ namespace ROS2CSMessageGenerator
 			Console.WriteLine ("Using message file name as message name: " + Name);
 
 			//Console.WriteLine ("Preparing class");
+			StructName = Name + "_t";
+			PrepareClassString ();
+			PrepareWrapperClassString ();
 
+		
+
+		}
+		public void PrepareClassString()
+		{
 			ClassString.AppendLine ("using System;");
 			ClassString.AppendLine ("using rclcs;");
 			ClassString.AppendLine ("using System.Runtime.InteropServices;");
@@ -52,10 +74,10 @@ namespace ROS2CSMessageGenerator
 			ClassString.AppendLine ("    [StructLayout (LayoutKind.Sequential)]");
 
 			if(!IsService)
-				ClassString.AppendLine ("    public struct " + Name + ":IRosMessage");
+				ClassString.AppendLine ("    public struct " + StructName + ":IRosMessage");
 			else
-				ClassString.AppendLine ("    public struct " + Name + ":IRosService");
-			
+				ClassString.AppendLine ("    public struct " + StructName + ":IRosService");
+
 			ClassString.AppendLine ("    {");
 			if (!IsService) {
 				ClassString.AppendLine ("        [DllImport (\"lib" + Namespace + "__rosidl_typesupport_introspection_c.so\")]");
@@ -67,32 +89,49 @@ namespace ROS2CSMessageGenerator
 			}
 			ClassString.AppendLine ("");
 			ClassString.AppendLine ("        public void Free(){\n" +
-			"          foreach (var item in this.GetType().GetFields()) {\n" +
-			"            if (typeof(IRosTransportItem).IsAssignableFrom (item.FieldType)) {\n" +
-			"               IRosTransportItem ros_transport_item = (IRosTransportItem)item.GetValue(this);\n" +
-			"               ros_transport_item.Free();\n" +
-			"            }\n"+
-			"          }\n"+
-			"        }"
+				"          foreach (var item in this.GetType().GetFields()) {\n" +
+				"            if (typeof(IRosTransportItem).IsAssignableFrom (item.FieldType)) {\n" +
+				"               IRosTransportItem ros_transport_item = (IRosTransportItem)item.GetValue(this);\n" +
+				"               ros_transport_item.Free();\n" +
+				"            }\n"+
+				"          }\n"+
+				"        }"
 			);
+		}
+		public void PrepareWrapperClassString()
+		{
+			//WrapperClassString.AppendLine ("using System;");
+			//WrapperClassString.AppendLine ("using rclcs");
+			//WrapperClassString.AppendLine ("using System.Runtime.InteropServices;");
+			WrapperClassString.AppendLine ("namespace " + Namespace);
+			WrapperClassString.AppendLine ("{");
 
-
-
-				
-					
-					
-
-				
-
-
-
-		
+			if(!IsService)
+				WrapperClassString.AppendLine ("    namespace msg");
+			else
+				WrapperClassString.AppendLine ("    namespace srv");
+			WrapperClassString.AppendLine ("    {");
+			WrapperClassString.AppendLine ("        public class " + Name + ":MessageWrapper<"+StructName+">");
+			WrapperClassString.AppendLine ("        {");
+			WrapperClassString.AppendLine ("           private bool disposed = false;");
+			WrapperClassString.AppendLine ("");
+			WrapperClassString.AppendLine ("           protected override void Dispose(bool disposing)");
+			WrapperClassString.AppendLine ("           {");
+			WrapperClassString.AppendLine ("               if (disposed)");
+			WrapperClassString.AppendLine ("                  return; ");
+			WrapperClassString.AppendLine ("               if (disposing) { ");
+			WrapperClassString.AppendLine ("                  data.Free(); ");
+			WrapperClassString.AppendLine ("               }");
+			WrapperClassString.AppendLine ("               disposed = true;");
+			WrapperClassString.AppendLine ("           }");
+			WrapperClassString.AppendLine ("");
 
 		}
 		public string GetResultingClass()
 		{
 			if (!ClassWasFinalized)
 				throw new Exception ("Class wasn't finalized");
+			ClassString.Append (WrapperClassString.ToString ());
 			return ClassString.ToString ();
 			
 		}
@@ -118,6 +157,11 @@ namespace ROS2CSMessageGenerator
 							//memberName += " = new rosidl_generator_c__primitive_array_" + nativeType + "()";
 							//Console.WriteLine ("Adding member of type: " + csType + " with name: " + memberName);
 							Members.Add ("public " + csType + " " + memberName + ";");
+							MessageMember member = new MessageMember ();
+							member.default_init = "";
+							member.name = memberName;
+							member.type = csType;
+							MessageMembers.Add (member);
 						}
 					} else if (IsPrimitiveType (splitted [0])) {
 						string csType = GetPrimitiveType (splitted [0]);
@@ -126,15 +170,23 @@ namespace ROS2CSMessageGenerator
 							memberName = memberName.Split (new char[]{ '=' }) [0];
 
 						}
-						if (csType == "rosidl_generator_c__String") {
+						/*if (csType == "rosidl_generator_c__String") {
 							//memberName += "= new rosidl_generator_c__String()";
 							Members.Add ("public " + csType + " " + memberName + ";");
 						} else if (csType == "System.Boolean") {
 							
 							Members.Add ("[MarshalAs(UnmanagedType.U1)]\n        public " + csType + " " + memberName + ";");
-						} else {
+						} else {*/
 							Members.Add ("public " + csType + " " + memberName + ";");
-						}
+
+						MessageMember member = new MessageMember ();
+						member.default_init = "";
+						member.name = memberName;
+						member.type = csType;
+						member.isArray = true;
+						MessageMembers.Add (member);
+
+						//}
 						//Console.WriteLine ("Adding member of type: " + csType + " with name: " + memberName);
 
 					} else if(splitted[0].Contains("/")){
@@ -144,6 +196,12 @@ namespace ROS2CSMessageGenerator
 						string pureType = nestedSplitted [1];
 						string nestedNamespace = nestedSplitted [0];
 						Members.Add ("public " +nestedNamespace+".msg."+ pureType + " " + splitted [1] + ";"); 
+
+						MessageMember member = new MessageMember ();
+						member.default_init = "";
+						member.name =  splitted [1];
+						member.type = nestedNamespace+".msg."+ pureType;
+						MessageMembers.Add (member);
 
 					}
 							
@@ -210,12 +268,35 @@ namespace ROS2CSMessageGenerator
 
 		public void FinalizeClass()
 		{	
-			foreach (var item in Members) {
+			/*foreach (var item in Members) {
 				ClassString.AppendLine ("        " + item);
+				//WrapperClassString.AppendLine
+			}*/
+			foreach (var item in MessageMembers) {
+				ClassString.AppendLine ("         " + item.ToString ());
+				WrapperClassString.AppendLine ("        public " + item.type + " "+ item.name);
+				WrapperClassString.AppendLine ("        {");
+				WrapperClassString.AppendLine ("            get{return data."+item.name+";}");
+				switch (item.type) {
+				case "string":
+					break;
+				case "float":
+					break;
+				case "double":
+					break;
+				default:
+					WrapperClassString.AppendLine ("            set{data." + item.name + " = value;}");
+					break;
+				}
+				WrapperClassString.AppendLine ("        }");
 			}
 			ClassString.AppendLine ("    }");
 			ClassString.AppendLine ("    }");
 			ClassString.AppendLine ("}");
+
+			WrapperClassString.AppendLine ("        }");
+			WrapperClassString.AppendLine ("    }");
+			WrapperClassString.AppendLine ("}");
 			ClassWasFinalized = true;
 		}
 		public string GetTypeSupportMessageFunctionName()
