6fd42bd39f138a5798baa9086806269ecf257ed6
firesurfer <firesurfer65@yahoo.de>
2017-08-09 18:29:52 +0200

    Added comments and made sure the windows branch works still on linux

--- src/Program.cs
+++ src/Program.cs
@@ -21,15 +21,17 @@ namespace ROS2CSMessageGenerator
 			
 
 		}
-
+		//TODO increase perfomance by parsing all messages in a package at once
 		public static void Main (string[] args)
 		{
 			bool IsService = false;
+			//Check the amount of arguments
 			if (args.Length < 1) {
 				PrintHelp ();
 				return;
 			}
 			if (args [0] == "-m") {
+				//-m means we want te generate messages
 				if (args.Length < 4) {
 					PrintHelp ();
 					return;
@@ -37,60 +39,74 @@ namespace ROS2CSMessageGenerator
 				string messageFile = args [1];
 				string packageName = args [2];
 				string outputPath = args [3];
+				//Check if the output paths exists
 				if (!Directory.Exists (outputPath))
 					Directory.CreateDirectory (outputPath);
 
 				Console.ForegroundColor = ConsoleColor.Blue;
 				Console.WriteLine ("Parsing message file: " + messageFile);
-				Console.ResetColor();
+				Console.ResetColor ();
+				//Determine if we are processing a service
 				if (messageFile.Contains ("Request") || messageFile.Contains ("Response")) {
 					IsService = true;
 				}
+				//We dont need to process to srv file. Just the two messages resulting by the srv file
 				if (Path.GetExtension (messageFile) == ".srv")
 					return;
-				
+
+				//Get the message name
 				string name = Path.GetFileName (messageFile);
+				//Remove the extenstion
 				name = name.Replace (Path.GetExtension (messageFile), "");
+				//Generate a message description
 				MessageDescription description = new MessageDescription (messageFile, packageName);
+				//Parse the message
 				MessageParser parser = new MessageParser (description);
 				try {
-					parser.Parse();
+					parser.Parse ();
 				} catch (Exception ex) {
+					//Parsing went wrong
 					Console.ForegroundColor = ConsoleColor.Red;
 					Console.WriteLine ("Exception parsing: " + messageFile);
 					Console.ResetColor ();
 					Console.WriteLine (ex.ToString ());
 					Environment.Exit (1);
 				}
-
+				//Generate code from the description
 				IMessageCodeGenerator codeGenerator = new CodeDomMessageGenerator ();
 				codeGenerator.GenerateCode (description);
 
-				//CsClassGenerator generator = new CsClassGenerator (messageFile, packageName);
-				//generator.Parse ();
-				//generator.FinalizeClass ();
-				//Console.WriteLine (generator.GetResultingClass ());
+				//Write the generated code to a text file
 				if (!IsService)
 					System.IO.File.WriteAllText (Path.Combine (outputPath, description.Name + "_msg.cs"), codeGenerator.GetGeneratedCode ());
 				else
 					System.IO.File.WriteAllText (Path.Combine (outputPath, description.Name + "_srv.cs"), codeGenerator.GetGeneratedCode ());
-			} else if (args [0] == "-c") {
+			} 
+			else if (args [0] == "-c") {
+				//-c means we want to compile a message package
 				if (args.Length < 3) {
 					PrintHelp ();
 					return;
 				}
+				//The directory the .cs files lay in
 				string classDir = args [1];
-                classDir = Path.GetFullPath(classDir);
-                Console.WriteLine(classDir);
+				//This converts the / to \ on windows and leaves them / on linux
+				classDir = Path.GetFullPath (classDir);
+				Console.WriteLine (classDir);
                
+				//The path we want to place the resulting assembly in
 				string assemblyPath = args [2];
-                assemblyPath = Path.GetFullPath(assemblyPath);
-                Console.WriteLine("Assembly Path: " + assemblyPath);
-              
+				assemblyPath = Path.GetFullPath (assemblyPath);
+				Console.WriteLine ("Assembly Path: " + assemblyPath);
+              	
+				//The passed directory did not exists
 				if (!Directory.Exists (classDir)) {
+					Console.ForegroundColor = ConsoleColor.Red;
 					Console.WriteLine ("Directory does not exist: " + classDir);
+					Console.ResetColor ();
 					return;
 				}
+				//Get all C# files in the directory
 				List<string> cs_files = new List<string> ();
 				foreach (var item in Directory.GetFiles(classDir)) {
 					FileInfo info = new FileInfo (item);
@@ -98,11 +114,13 @@ namespace ROS2CSMessageGenerator
 						cs_files.Add (item);
 					}
 				}
+				//Compile them
 				CompileToAssembly (assemblyPath, cs_files);
-			} else {
+			} 
+			else {
 				PrintHelp ();
 			}
-			Console.WriteLine("");
+			Console.WriteLine ("");
 
 		}
 
@@ -110,58 +128,61 @@ namespace ROS2CSMessageGenerator
 		{
 
 			CompilerParameters cp = new CompilerParameters ();
+			//We need to allow unsafe code
 			cp.CompilerOptions += " /unsafe ";
 
-		
+			string AssemblyName = Path.GetFileNameWithoutExtension (AssemblyPath);
 			CSharpCodeProvider provider = new CSharpCodeProvider ();
 
-
+			//Retrieve search paths from the AMENT_PREFIX_PATH
 			string rclcsPath = Environment.GetEnvironmentVariable ("AMENT_PREFIX_PATH");
-            Console.WriteLine("Ament Prefix Path: " + rclcsPath);
-            string[] pathElements;
-            if (Environment.OSVersion.Platform == PlatformID.Unix)
-            {
-                pathElements = rclcsPath.Split(new char[] { ':' });
-            }
-            else
-            {
-                pathElements = rclcsPath.Split(new char[] { ';' });
-            }
-            //rclcsPath = firstPathElement;
-            cp.ReferencedAssemblies.Add("System.dll");
-            foreach (var pathElement in pathElements)
-            {
+			Console.WriteLine ("Ament Prefix Path: " + rclcsPath);
 
+			string[] pathElements;
+			//On linux paths variables are seperated by : on windows by ;
+			if (Environment.OSVersion.Platform == PlatformID.Unix) {
+				pathElements = rclcsPath.Split (new char[] { ':' });
+			} else {
+				pathElements = rclcsPath.Split (new char[] { ';' });
+			}
 
-                string ros2libPath = Path.Combine(pathElement, "lib");
-              
-                Console.WriteLine("ros2 libs path: " + ros2libPath);
-                foreach (var item in Directory.GetFiles(ros2libPath))
-                {
-                    if (Path.GetExtension(item) == ".dll")
-                    {
-                        try
-                        {
-                            System.Reflection.AssemblyName testAssembly = System.Reflection.AssemblyName.GetAssemblyName(item);
-                            Console.WriteLine(testAssembly.FullName);
-                            cp.ReferencedAssemblies.Add(item);
+			//We need to reference the System.dll
+			cp.ReferencedAssemblies.Add ("System.dll");
 
-                        }
-                        catch (Exception ex)
-                        {
+			//For all elements in the AMENT_PREFIX_PATH
+			foreach (var pathElement in pathElements) {
 
-                        }
+				//Look into the lib folder
+				//TODO on windows look into bin folder?
+				string ros2libPath = Path.Combine (pathElement, "lib");
+   				
+				Console.WriteLine ("ros2 libs path: " + ros2libPath);
+				//Check the files
+				foreach (var item in Directory.GetFiles(ros2libPath)) {
+					//A dll could be an assembly
+					if (Path.GetExtension (item) == ".dll") {
+						try {
+							//Try loading the assembly -> if it works it is an assembly if not it's just normal dll
+							System.Reflection.AssemblyName testAssembly = System.Reflection.AssemblyName.GetAssemblyName (item);
+							Console.WriteLine (testAssembly.FullName);
+							if(testAssembly.Name != AssemblyName)
+								cp.ReferencedAssemblies.Add (item);
 
-                    }
-                }
-            }
+						} catch (Exception ex) {
 
+						}
+
+					}
+				}
+			}
+			//messages allways should result in a library
 			cp.GenerateExecutable = false;
 			cp.OutputAssembly = AssemblyPath;
-
 			cp.GenerateInMemory = false;
+
 			try {
-				CompilerResults results = Compile(cp, files, provider);
+				//Compile it
+				CompilerResults results = Compile (cp, files, provider);
 
 				if (results.Errors.Count > 0) {
 
@@ -170,7 +191,7 @@ namespace ROS2CSMessageGenerator
 					Console.WriteLine ("Errors/Warnings building: " + AssemblyPath);
 					Console.ResetColor ();
 
-					Console.WriteLine ("Search path for rclcs was: " + rclcsPath);
+					Console.WriteLine ("AMENT_PREFIX_PATH was: " + rclcsPath);
 					foreach (CompilerError ce in results.Errors) {
 						if (ce.IsWarning) {
 							Console.ForegroundColor = ConsoleColor.Blue;
@@ -193,40 +214,12 @@ namespace ROS2CSMessageGenerator
 
 				}
 			} catch (Exception ex) {
-				Console.WriteLine ("Using fallback without /nowarn option");
-				cp.CompilerOptions = "/unsafe";
-				CompilerResults results = Compile(cp, files, provider);
-
-				if (results.Errors.Count > 0) {
-
-					// Display compilation errors.
-					Console.ForegroundColor = ConsoleColor.Red;
-					Console.WriteLine ("Errors/Warnings building: " + AssemblyPath);
-					Console.ResetColor ();
-
-					Console.WriteLine ("Search path for rclcs was: " + rclcsPath);
-					foreach (CompilerError ce in results.Errors) {
-						if (ce.IsWarning) {
-							Console.ForegroundColor = ConsoleColor.Blue;
-							Console.WriteLine (ce.FileName + " " + ce.ErrorNumber);
-							Console.ResetColor ();
-							Console.WriteLine ("  {0}", ce.ToString ());
-							Console.WriteLine ();
-						} else {
-
-							Console.ForegroundColor = ConsoleColor.DarkRed;
-							Console.WriteLine (ce.FileName + " " + ce.ErrorNumber);
-							Console.ResetColor ();
-							Console.WriteLine ("  {0}", ce.ToString ());
-							Console.WriteLine ();
-						}
-					}
-				}
-
+				Console.WriteLine (ex.ToString ());
 			}
 
 		}
-		public static CompilerResults Compile(CompilerParameters cp, List<String> files, CSharpCodeProvider provider)
+
+		public static CompilerResults Compile (CompilerParameters cp, List<String> files, CSharpCodeProvider provider)
 		{
 			return  provider.CompileAssemblyFromFile (cp, files.ToArray ());
 		}
